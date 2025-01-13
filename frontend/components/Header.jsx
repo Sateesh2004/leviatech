0c2b2b02de6b90d60a172459b26ee0ac5ffbca66
Sateesh2004 <kumarsateesh838@gmail.com>
2025-01-13 06:47:23 +0530

    "Final Commit"

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -5,10 +5,10 @@ const Header = () => {
   return (
     <header className=" bg-blue-100">
     <div className="max-w-7xl  mx-auto px-4 py-4 flex flex-col gap-2 sm:flex-row  items-center justify-between">
-      <div className="flex items-center p-5 border rounded-xl   bg-white font-bold text-[24px] ">
+      <div className="flex items-center w-[100%] sm:w-auto p-5 border rounded-xl   bg-white font-bold text-[20px] lg:text-[24px] ">
       Narayana Nethralay
       </div>
-      <div className="flex items-center md:flex-row flex-col  p-4 rounded-xl border bg-white sm:gap-6">
+      <div className="flex items-center w-[100%] sm:w-auto md:flex-row flex-col  p-4 rounded-xl border bg-white sm:gap-6">
         <div className="mr-8">
         <h1 className="text-xl lg:block hidden font-bold text-[24px] text-gray-800">
         Narayana Eye Hospital
@@ -16,13 +16,15 @@ const Header = () => {
         </div>
         <div className="relative mr-8 border-b-2">
           <Search className="absolute left-1 top-1/2 -translate-y-1/2 text-gray-400 h-7 w-7" />
+          
           <input
             type="text"
             placeholder="Search"
             className="pl-10 pr-4 py-2 rounded-lg  border-gray-200 focus:outline-none w-64"
           />
         </div>
-        <div className="flex text-center sm:text-right flex-col ">
+        
+        <div className="flex text-center sm:text-right flex-col mt-2 ">
           <span className="font-medium">Admin</span>
           <span className="text-sm text-gray-500">hello@samwhitaker.com</span>
         </div>
